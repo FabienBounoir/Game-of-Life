82e6738dbfa15a9b81034bd93a8a817be2a1c422
FabienBounoir <bounoirfabien@gmail.com>
2024-04-27 22:26:46 +0200

    feat: 🎸 Add vercel statistics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,10 @@
 	import '../app.scss';
 	import myshades from '$lib/myshades';
 
+	import { inject } from '@vercel/analytics';
+
 	onMount(async () => {
+		inject();
 		let color = Math.floor(Math.random() * 16777215).toString(16);
 
 		if (color.length < 6) color = '0'.repeat(6 - color.length) + color;
